611a9dfed2dda0114a9e4fa5d99169ce149925b7
your_name <ruth.kyx@gmail.com>
2023-12-03 23:10:06 +0800

    day16 in class demo

--- day16/day16pm/src/main/java/ssf/iss/nus/day16pm/controllers/TaskController.java
+++ day16/day16pm/src/main/java/ssf/iss/nus/day16pm/controllers/TaskController.java
@@ -1,17 +1,29 @@
 package ssf.iss.nus.day16pm.controllers;
 
-import org.springframework.stereotype.Controller;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping(path="/")
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+
+@RestController
+@RequestMapping(path="/api/task", produces="")
 public class TaskController {
     
-    @PostMapping
-    public String getTask() {
+    @PostMapping(consumes="")
+    public ResponseEntity<String> getTaskAsJSON() {
+        JsonObject resp = Json.createObjectBuilder()
+            .add("id", UUID.randomUUID().toString())
+            .add("createTime", (new Date().getTime()))
+            .build();
 
-        return "task";
+        return ResponseEntity.ok(resp.toString());
     }
+
  
-}
+}  
---
+++ day16/day16pm/src/main/java/ssf/iss/nus/day16pm/services/TaskServices.java
@@ -0,0 +1,8 @@
+package ssf.iss.nus.day16pm.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskServices {
+    
+}
