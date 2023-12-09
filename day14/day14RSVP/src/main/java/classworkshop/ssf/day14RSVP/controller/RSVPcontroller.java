354054d2dc77453929e3a229fff30a20424feb00
your_name <ruth.kyx@gmail.com>
2023-12-09 10:28:48 +0800

    rsvp controller html pages updated

--- day14/day14RSVP/src/main/java/classworkshop/ssf/day14RSVP/controller/RSVPcontroller.java
+++ day14/day14RSVP/src/main/java/classworkshop/ssf/day14RSVP/controller/RSVPcontroller.java
@@ -2,9 +2,12 @@ package classworkshop.ssf.day14RSVP.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,14 +15,19 @@ import org.springframework.web.servlet.ModelAndView;
 public class RSVPcontroller {
     
     @GetMapping
-    public String getRSVP(Model model) {
-        model.addAttribute("name", );
+    public String getRSVP(@RequestParam MultiValueMap<String, String> form,
+    @RequestParam String name, Model model) {
+
+        model.addAttribute("name", form.getFirst("name"));
         return "thankyou";
     }
 
     @PostMapping
-    public ModelAndView postRSVP(Model model) {
+    public ModelAndView postRSVP(@RequestBody MultiValueMap<String, String> form) {
         ModelAndView mav = new ModelAndView();
+        String name = form.getFirst("name").trim().toLowerCase();
+        mav.setViewName("thankyou");
+        mav.addObject("name", name);
 
         return mav;
     }
